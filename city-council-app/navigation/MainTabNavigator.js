2ebfec35de40e568bbb908a9ed855c1eec632693
Rishi Raj <rajminster@gmail.com>
2018-02-28 16:50:38 -0500

    Basic Tab Menu

--- city-council-app/navigation/MainTabNavigator.js
+++ city-council-app/navigation/MainTabNavigator.js
@@ -8,6 +8,7 @@ import Colors from '../constants/Colors';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import NotificationScreen from '../screens/NotificationScreen';
 
 export default TabNavigator(
   {
@@ -20,6 +21,9 @@ export default TabNavigator(
     Settings: {
       screen: SettingsScreen,
     },
+    Notifications: {
+      screen: NotificationScreen,
+    }
   },
   {
     navigationOptions: ({ navigation }) => ({
